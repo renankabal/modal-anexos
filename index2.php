700b412f2c7d255fd7311265875ff354c56ee99b
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-11 16:44:39 -0300

    Comitado para refatoramento

---
+++ index2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="bs-example bs-navbar-top-example" data-example-id="navbar-fixed-to-top">
+    <nav class="navbar navbar-default navbar-fixed-top navbar-right">
+      <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          	<img src="img/logoProescTopo.png" alt="">
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
+          <ul class="nav navbar-nav">
+            <li>   </li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Recursos</a></li>
+            <li><a href="#">Depoimentos</a></li>
+            <li><a href="#">Planos</a></li>
+            <li><a href="#">Entrar</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div>
+    </nav>
+  </div>
+
+</body>
+</html>
